14b1ed676d2ae034b8509b45a68ad4c2bc68477b
Hemanth N <hemanthn270@gmail.com>
2026-03-12 14:01:33 +0530

    Initial commit - React ChatBox project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+import MessageInput from './MessageInput'
+function App() {
+  const [texts, setTexts] = useState("")
+  const [message, setMessage] = useState([
+{
+  "id":1,
+"sender":"user",
+"message":"Hello"
+},
+{
+  "id":2,
+"sender":"robot",
+"message":"Hi...how can i help you?"
+},
+{
+  "id":3,
+"sender":"user",
+"message":"Flip a coin"
+},
+{
+  "id":4,
+"sender":"robot",
+"message":"You got Heads!"
+}
+
+])
+
+
+  return (
+       <div>
+        {console.log(message)}
+          <MessageInput 
+          texts={texts} 
+          setTexts={setTexts} 
+          message={message} 
+          setMessage={setMessage}/>
+       </div>
+  )
+}
+
+export default App
---
+++ src/MessageInput.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+
+const MessageInput = ({ message, setMessage, texts, setTexts }) => {
+  
+  const handleSend = (texts)=>{
+  try{
+    if(!texts.trim()) return;
+const textFormatted ={
+        id:crypto.randomUUID(),
+        sender:"user",
+        message:texts
+  } 
+  const updatedText = [...message, textFormatted]
+  setMessage(updatedText);
+  setTexts('')
+  console.log(message);
+   }catch(err){
+    console.log(err.message)
+  } 
+   
+
+}
+    return (
+    <div className='inputSec'>
+      <input  
+      type='text' 
+      className='msgInp' 
+      value={texts}
+      onChange={(e)=>setTexts(e.target.value)} />
+   <button 
+        className='messageBtn' 
+        onClick={()=>handleSend(texts)}
+        >send</button>
+    </div>
+  )
+}
+
+export default MessageInput
